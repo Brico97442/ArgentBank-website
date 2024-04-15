5a0b3c9a8f1ba5712acfd51aa012ced656a9dfa2
Brico97442 <137392526+Brico97442@users.noreply.github.com>
2024-04-15 19:55:24 +0400

    create feature container and map feature element

--- argent-bank/src/containers/features.jsx
+++ argent-bank/src/containers/features.jsx
@@ -1,25 +1,47 @@
 import React from "react";
 
 import "../styles/index.css";
-import picture from "../img/icon-chat.png"
+import chatIcon from "../img/icon-chat.png";
+import moneyIcon from "../img/icon-money.png";
+import securityIcon from "../img/icon-security.png";
 
-
+const featureData = [
+  {
+    id: 1,
+    icon: chatIcon,
+    title: "You are our #1 priority",
+    description:
+      " Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes.",
+    alt: "Chat Icon",
+  },
+  {
+    id: 2,
+    icon: moneyIcon,
+    title: "More savings means higher rates",
+    description:
+      " The more you save with us, the higher your interest rate will be!",
+    alt: "Money Icon",
+  },
+  {
+    id: 3,
+    icon: securityIcon,
+    title: "Security you can trust",
+    description:
+      "We use top of the line encryption to make sure your data and money is always safe.",
+    alt: "Security Icon",
+  },
+];
 
 export function Features() {
   return (
     <section className="features">
-      <div className="feature-item">
-        <img
-          src={picture}
-          alt="Chat Icon"
-          className="feature-icon"
-        />
-        <h3 className="feature-item-title">You are our #1 priority</h3>
-        <p>
-          Need to talk to a representative? You can get in touch through our
-          24/7 chat or through a phone call in less than 5 minutes.
-        </p>
-      </div>
+      {featureData.map((feature) => (
+        <div key={feature.id} className="feature-item">
+          <img src={feature.icon} alt={feature.alt} className="feature-icon" />
+          <h3 className="feature-item-title">{feature.title}</h3>
+          <p>{feature.description}</p>
+        </div>
+      ))}
     </section>
   );
 }
