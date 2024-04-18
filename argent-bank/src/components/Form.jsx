9fe7aadcfbb31a4163d764346dddc10efbbb0b80
Brico97442 <137392526+Brico97442@users.noreply.github.com>
2024-04-18 05:41:32 +0400

    test redux with post request

--- argent-bank/src/components/Form.jsx
+++ argent-bank/src/components/Form.jsx
@@ -1,11 +1,26 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useRef } from "react";
 
 import "../styles/index.css";
+import { useDispatch, useSelector } from "react-redux";
 
 export function Form() {
+  const form = useRef();
+  const user = useSelector((state) => state.userReducer);
+  const dispatch = useDispatch();
+
+  const handleForm = async (e) => {
+    e.preventDefault();
+    console.log(user);
+   
+    const postData = {
+      userName: form.current[0].value,
+      password:form.current[1].value
+    }
+
+    // dispatch(addPost(postData))
+  };
   return (
-    <form>
+    <form ref={form} onSubmit={(e) => handleForm(e)}>
       <div className="input-wrapper">
         <label htmlFor="username">Username</label>
         <input type="text" id="username" />
@@ -13,17 +28,14 @@ export function Form() {
 
       <div className="input-wrapper">
         <label htmlFor="password">Password</label>
-        <input type="password" id="password" />
+        <input type="current-password" id="password" />
       </div>
 
       <div className="input-remember">
         <input type="checkbox" id="remember-me" />
         <label htmlFor="remember-me">Remember me</label>
       </div>
-
-      <Link to="/User">
-        <button className="sign-in-button">Sign in</button>
-      </Link>
+      < input type="submit"  value="Sign in" className="sign-in-button"/>
     </form>
   );
 }
--- argent-bank/src/redux/actions/user.action.js
+++ argent-bank/src/redux/actions/user.action.js
@@ -5,7 +5,7 @@ export const POST_USER = "POST_USER";
 export const postUser = () => {
     return ( dispatch ) => {
         return axios.post("http://localhost:3001/api-docs/#/User%20Module/post_user_login").then((res) =>{
-        console.log(res)   
+        dispatch({type: POST_USER, payload :res.status});   
         })
     }
 }
