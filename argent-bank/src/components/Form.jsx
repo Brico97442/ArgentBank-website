92e0d57ee2b3582516d8982b6c22395a3dfa42f1
Brico97442 <137392526+Brico97442@users.noreply.github.com>
2024-04-21 14:52:07 +0400

    create login form logic

--- argent-bank/src/components/Form.jsx
+++ argent-bank/src/components/Form.jsx
@@ -1,46 +1,86 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import axios from "axios";
 import Button from "./Button";
 import "../styles/index.css";
+import { setSignIn } from "../redux/slice/userloginSlice";
 
+export function LoginForm() {
+  const redirection = useNavigate();
+  const dispatch = useDispatch();
 
-// redux State
-
-export function Form() {
-  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [remember, setRemember] = useState(false);
 
+  const submit = async (event) => {
+    event.preventDefault();
+    const formData = {
+      email: email,
+      password: password,
+    };
+    //* Envoie requête vers API pour connexion
+    try {
+      const response = await axios.post(
+        "http://localhost:3001/api/v1/user/login",
+        formData,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      if (response.status === 200) {
+        const responseData = response.data; // récup données
+        const token = responseData.body.token; // extract token auth
+        localStorage.setItem("authToken", token); // save token
+        dispatch(setSignIn({ token })); // envoie action au store (user connecté)
+        redirection("/User"); // redirection
+      } else {
+        setErrorMessage(response.statusText); // màj message erreur
+      }
+    } catch {
+      //* Gestion erreurs imprévues
+      setErrorMessage("An error as occured."); // màj message erreur
+    }
+  };
   return (
-    <form>
+    <form onSubmit={submit}>
+      {errorMessage && <p className="error-login">{errorMessage}</p>}
       <div className="input-wrapper">
         <label htmlFor="username">Username</label>
         <input
-          type="text"
+          content="email"
+          type="email"
           required
           id="username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
+          onChange={(e) => setEmail(e.target.value)}
         />
       </div>
-
       <div className="input-wrapper">
         <label htmlFor="password">Password</label>
         <input
-          type="current-password"
+          content="password"
+          type="password"
           required
           id="password"
-          value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
       </div>
       <div className="input-remember">
-        <input type="checkbox" id="remember-me" />
         <label htmlFor="remember-me">Remember me</label>
+        <input
+          type="checkbox"
+          id="remember-me"
+          onChange={() => setRemember(!remember)}
+          checked={remember}
+        />
       </div>
-      <Link to="/User"> 
-        <Button content="Sign In" btnClass="sign-in-button"/>
-      </Link>
+
+      <Button content="Sign In" btnClass="sign-in-button" />
     </form>
   );
 }
-export default Form;
+export default LoginForm;
--- argent-bank/src/redux/slice/UserProfileSlice.jsx
+++ argent-bank/src/redux/slice/UserProfileSlice.jsx
@@ -1,26 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-// import axios from "axios";
 
-// const getToken = () => {
-//   return localStorage.getItem("authToken") || null;
-// };
-
-// const initialState = {
-//   token: getToken(),
-// };
-
-// export const loginUser = createAsyncThunk(
-//   "user/loginUser",
-//   async (userCredentials) => {
-//     const request = await axios.post(
-//       "http://localhost:3001/api/v1/user/login",
-//       userCredentials
-//     );
-//     const response = await request.data;
-//     localStorage.setItem("user", JSON.stringify(response));
-//     return response;
-//   }
-// );
 
 const userprofileSlice = createSlice({
   name: "user",
