6d36ffba925f926a145e38659cbdc1f8167b66b9
Brico97442 <137392526+Brico97442@users.noreply.github.com>
2024-04-29 06:57:41 +0400

    edit sign in item for display userName

--- argent-bank/src/components/Sign-in-item.jsx
+++ argent-bank/src/components/Sign-in-item.jsx
@@ -7,12 +7,12 @@ export default function SigninItem({ className }) {
   const userProfile = useSelector((state)=> state.user)
   console.log(userProfile)
   const isConnected = useSelector((state) => state.login.isConnected);    // Accéder à l'état d'authentification depuis le store Redux
-  const firstName = userProfile.firstName
+  const userName = userProfile.userName
   
   return (
     <>
       <i className="fa fa-user-circle"></i>
-      <h1 className={className}>{isConnected ? firstName : "Sign In"}</h1>
+      <h1 className={className}>{isConnected ? userName : "Sign In"}</h1>
     </>
   );
 }
