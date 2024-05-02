dd0870466fd1a7b4bb933bd969671a21870c69ce
Brico97442 <137392526+Brico97442@users.noreply.github.com>
2024-05-03 01:02:04 +0400

    reorganize sign in css

--- frontend/src/pages/Sign-in.jsx
+++ frontend/src/pages/Sign-in.jsx
@@ -1,10 +1,8 @@
 import React from "react";
 
 import Form from "../components/Login-form";
-
 import UserItem from "../components/Useritem";
 
-import "../styles/index.css";
 import "../styles/sign-in.css";
 
 export default function Signin() {
